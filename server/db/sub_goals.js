f852813131b4333b5dd082803db5b9982b1d232b
thongchai-hon <hon.man@hotmail.com>
2022-05-06 11:53:11 +1200

    add subgoals and tasks get function

--- server/db/sub_goals.js
+++ server/db/sub_goals.js
@@ -1,3 +1,12 @@
+//FUNCTIONS RELATED TO sub_goals DB
+
 const connection = require('./connection')
 
 //GET sub_goals data by goal_id
+function getSubGoals(subgoal_id, db = connection) {
+  return db('goals').where('goal_id', subgoal_id).select
+}
+
+module.exports = {
+  getSubGoals,
+}
--- server/db/tasks.js
+++ server/db/tasks.js
@@ -1,3 +1,12 @@
+//FUNCTIONS RELATED TO TASKS DB
+
 const connection = require('./connection')
 
 //GET tasks data by goal_id
+function getTasks(tasks_id, db = connection) {
+  return db('goals').where('goal_id', tasks_id).select
+}
+
+module.exports = {
+  getTasks,
+}
