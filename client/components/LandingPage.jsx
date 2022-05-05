47edcb555d54efc5a42717c589aacebba1cacfbe
Min Khanthee <pkhanthee@gmail.com>
2022-05-05 16:42:44 +1200

    added draft landing page

---
+++ client/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <>
+      <h1>PlanBuddy</h1>
+      <h2>Helping you with the planning so you can focus on the learning!</h2>
+    </>
+  )
+}
+
+export default LandingPage
