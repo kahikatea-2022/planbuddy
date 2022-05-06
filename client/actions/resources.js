e8cc974d1a2e94e1648f67a369744beebbff7376
Min Khanthee <pkhanthee@gmail.com>
2022-05-06 21:53:51 +1200

    complete resource form, need to complete task

--- client/actions/resources.js
+++ client/actions/resources.js
@@ -1,5 +1,5 @@
 const SET_RESOURCES = 'SET_RESOURCES'
-const ADD_RESOURCE = 'ADD_RESOURCES'
+const ADD_RESOURCE = 'ADD_RESOURCE'
 
 export function setResources(resources) {
   return {
--- client/components/CreateSubGoal.jsx
+++ client/components/CreateSubGoal.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { addGoal, ADD_GOAL, fetchGoals } from '../actions/goals'
 import { useNavigate } from 'react-router-dom'
 import { addResource } from '../actions/resources'
-import { addTask } from '../actions/tasks'
+import { addTask, setTasks } from '../actions/tasks'
 
 //Steps:
 //Create an add resources form
@@ -17,33 +17,44 @@ function CreateSubGoal() {
   const dispatch = useDispatch()
   // const navigate = useNavigate()
 
-  const resources = [
-    {
-      resource_name: 'youtube',
-      url: 'www.youtube.com',
-    },
-    {
-      resource_name: 'udemy',
-      url: 'www.udemy.com',
-    },
-  ]
+  // const resources = [
+  //   {
+  //     resource_name: 'youtube',
+  //     url: 'www.youtube.com',
+  //   },
+  //   {
+  //     resource_name: 'udemy',
+  //     url: 'www.udemy.com',
+  //   },
+  // ]
 
-  const tasks = [
-    {
-      subgoal_name: 'learn C major scale',
-      completed: false,
-      current: true,
-    },
-    {
-      subgoal_name: 'learn D major scale',
-      completed: false,
-      current: false,
-    },
-  ]
+  // const tasks = [
+  //   {
+  //     subgoal_name: 'learn C major scale',
+  //     completed: false,
+  //     current: true,
+  //   },
+  //   {
+  //     subgoal_name: 'learn D major scale',
+  //     completed: false,
+  //     current: false,
+  //   },
+  // ]
 
-  const [inputStateResources, setInputStateResources] = useState(resources)
+  const resources = useSelector((state) => state.resources)
+  const tasks = useSelector((state) => state.tasks)
+  console.log(resources)
 
-  const [inputStateTasks, setInputStateTasks] = useState(tasks)
+  const [inputStateResources, setInputStateResources] = useState({
+    resource_name: '',
+    url: '',
+  })
+
+  const [inputStateTasks, setInputStateTasks] = useState({
+    name: '',
+    completed: false,
+    current: false,
+  })
 
   const handleFormResources = (event) => {
     setInputStateResources({
@@ -58,36 +69,54 @@ function CreateSubGoal() {
       ...inputStateTasks,
       [event.target.id]: event.target.value,
     })
-    console.log(inputStateTasks)
   }
 
   function submitHandlerResources(event) {
-    if (event.code === 'Enter') {
-      dispatch(addResource(inputStateResources))
-      console.log('submitHandler')
-    }
+    event.preventDefault()
+    dispatch(addResource(inputStateResources))
   }
 
   function submitHandlerTasks(event) {
-    if (event.code === 'Enter') {
-      dispatch(addTask(inputStateTasks))
-      console.log('submitHandlerTasks')
-    }
+    event.preventDefault()
+    dispatch(addTask(inputStateTasks))
   }
 
+  // useEffect(() => {
+  //   dispatch(setTasks())
+  // }, [])
+  // console.log('resourcestate', resources.resource_name)
   return (
     <>
       <h1>Create SubGoal</h1>
       <form>
         <label htmlFor="resource_name">Resources for your learning:</label>
-        <input type="text" onChange={handleFormResources}></input>
+        <input
+          type="text"
+          id="resource_name"
+          onChange={handleFormResources}
+          value={inputStateResources.resource_name}
+        ></input>
         <label htmlFor="url">Link to resource:</label>
-        <input type="text" onChange={handleFormResources}></input>
+        <input
+          type="text"
+          id="url"
+          onChange={handleFormResources}
+          value={inputStateResources.url}
+        ></input>
         <button onClick={submitHandlerResources}>Add</button>
       </form>
+      {/* list to render the resources*/}
+      {/* <ul>
+        <li>{resources.resource_name}</li>
+      </ul> */}
       <form>
         <h2>Great work, now add your first tasks</h2>
-        <input type="text" onChange={handleFormTasks}></input>
+        <input
+          type="text"
+          id="task"
+          value={inputStateTasks.name}
+          onChange={handleFormTasks}
+        ></input>
         <button onClick={submitHandlerTasks}>Add</button>
       </form>
     </>
