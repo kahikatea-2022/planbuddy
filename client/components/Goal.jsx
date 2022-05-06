b5e69ebbd6ddc3c851dfd6eadb100e4175601952
Karmel Lithgow <karmel.lithgow@gmail.com>
2022-05-06 14:13:54 +1200

    added ability to use state

--- client/components/Goal.jsx
+++ client/components/Goal.jsx
@@ -1,37 +1,37 @@
 import { format } from 'prettier'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { addGoal, ADD_GOAL } from '../actions/goals'
+import { addGoal, ADD_GOAL, fetchGoals } from '../actions/goals'
 import { useNavigate } from 'react-router-dom'
 
 function Goal() {
-  const exampleData = [
-    {
-      goal_name: 'Piano',
-      why: 'Play at friends wedding',
-      weekly_hours: '20',
-    },
-  ]
+  // const exampleData = {
+  //   goal_name: 'Piano',
+  //   why: 'Play at friends wedding',
+  //   weekly_hours: '20',
+  // }
 
-  const goal = useSelector((state) => state.goal)
-  console.log(goal)
+  const goals = useSelector((state) => state.goals)
+  console.log(goals)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchGoals())
+  }, [])
 
   return (
-    <>
-      {exampleData.map((exampleData) => {
-        return (
-          <>
-            <h1> Learning Plan </h1>
-            <p> I want to learn {exampleData.goal_name}</p>
-            <p> So that I can {exampleData.why}</p>
-            <p>
-              I will dedicate {exampleData.weekly_hours} hours per week to
-              achieve this goal
-            </p>
-          </>
-        )
-      })}
-    </>
+    goals.length !== 0 && (
+      <>
+        <h1> Learning Plan </h1>
+        <p> I want to learn {goals[0].goal_name}</p>
+        <p> So that I can {goals[0].why}</p>
+        <p>
+          I will dedicate {goals[0].weekly_hours} hours per week to achieve this
+          goal
+        </p>
+      </>
+    )
   )
 }
 
