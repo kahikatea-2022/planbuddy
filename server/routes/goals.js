217dff07c8ac0ca92290decb3c3667cdcd962546
williams-z <williams.z.bis@gmail.com>
2022-05-06 15:41:54 +1200

    created goals post route to add new goal

--- server/routes/goals.js
+++ server/routes/goals.js
@@ -6,19 +6,31 @@ const goals = require('../db/goals')
 const router = express.Router()
 
 // GET /api/v1/plans/
-
 router.get('/', (req, res) => {
   let id = 1
-  goals
-    .getUserPlans(id)
+  goals.getGoalDataById(id)
     .then((data) => {
       res.json(data)
       return null
     })
     .catch((err) => {
       console.error(err)
-      res.status(500).json({ message: 'Something went wrong' })
+      res.status(500).json({ message: 'Something went wrong getting goal data by ID' })
     })
+})
+
+// POST /api/v1/plans/
+router.post('/', (req, res) => {
+  const goalData = req.body
+  goals.addNewGoal(goalData)
+  .then(() => {
+    res.sendStatus(200)
+    return null
+  })
+  .catch((err) => {
+    console.error(err)
+    res.status(500).json({ message: 'Something went wrong adding goal to the database' })
+  })
 })
 
 module.exports = router
