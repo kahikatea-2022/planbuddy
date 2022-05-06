ef7ac0043f6cc8a998dfae8cc219f83b6726635e
williams-z <williams.z.bis@gmail.com>
2022-05-06 14:58:30 +1200

    debugged db functions

--- server/db/sub_goals.js
+++ server/db/sub_goals.js
@@ -4,7 +4,7 @@ const connection = require('./connection')
 
 //GET sub_goals data by goal_id
 function getSubGoals(goal_id, db = connection) {
-  return db('goals').where('goal_id', goal_id).select()
+  return db('sub_goals').where('goal_id', goal_id).select()
 }
 
 module.exports = {
--- server/db/tasks.js
+++ server/db/tasks.js
@@ -4,7 +4,7 @@ const connection = require('./connection')
 
 //GET tasks data by goal_id
 function getTasks(goal_id, db = connection) {
-  return db('goals').where('goal_id', goal_id).select()
+  return db('tasks').where('goal_id', goal_id).select()
 }
 
 module.exports = {
