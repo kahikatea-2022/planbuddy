9efff84de1ebd6dc185b7d3eb9316f163fd1722c
Sindre <sinfr91@gmail.com>
2022-05-06 15:04:43 +1200

    add get subgoals by id api call

--- client/apis/subGoals.js
+++ client/apis/subGoals.js
@@ -3,24 +3,25 @@ import request from 'superagent'
 const rootUrl = '/api/v1'
 
 // ?get subgoals by goal id
-export function getTasks() {
-  return Promise.resolve([
-    { id: 1, name: 'catch their attention', completed: false, current: true },
-    {
-      id: 2,
-      name: 'learn about their interests',
-      completed: false,
-      current: false,
-    },
-    {
-      id: 3,
-      name: 'have a conversation with them',
-      completed: true,
-      current: false,
-    },
-  ]).then((res) => res)
+export function getSubGoals(goal_id) {
+  // return Promise.resolve([
+  //   { id: 1, name: 'catch their attention', completed: false, current: true },
+  //   {
+  //     id: 2,
+  //     name: 'learn about their interests',
+  //     completed: false,
+  //     current: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'have a conversation with them',
+  //     completed: true,
+  //     current: false,
+  //   },
+  // ]).then((res) => res)
 
-  // return request.get(rootUrl + '/tasks').then((res) => {
-  //   return res.body.users
-  // })
+  // check URL when routes are ready
+  return request.get(rootUrl + `/subgoal/` + goal_id).then((res) => {
+    return res.body
+  })
 }
