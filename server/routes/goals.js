0a490566fc9be6848a3618eac910bf87168243a4
thongchai-hon <hon.man@hotmail.com>
2022-05-06 12:15:59 +1200

    add routes

---
+++ server/routes/goals.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const jwtAuthz = require('express-jwt-authz')
+const { getUserRoles, checkJwt } = require('../auth0')
+
+//these are the variables to acces the DB functions by table name
+const goals = require('../db/goals')
+
+const router = express.Router()
+
+// middleware for checking permissions (authorization)
+const checkAdmin = jwtAuthz(['read:my_private_route'], {
+  customScopeKey: 'permissions',
+})
+
+// GET /api/v1/plans/
+router.get('/', (req, res) => {
+  let userPlan = {}
+
+  goals
+    .getGoalData(1)
+    .then((data) => {
+      userPlan.goalData = data
+    })
+    .then(() => {
+      res.json(userPlan)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
+})
+
+module.export = router
