01a4edc4a44ac1c3c0ebd4d263f9d590db09304e
Sindre <sinfr91@gmail.com>
2022-05-07 15:03:45 +1200

    add resources api stack

--- client/actions/resources.js
+++ client/actions/resources.js
@@ -1,5 +1,7 @@
+import { getResourcesBySubGoalId } from '../apis/resources'
+
 const SET_RESOURCES = 'SET_RESOURCES'
-const ADD_RESOURCE = 'ADD_RESOURCES'
+const ADD_RESOURCE = 'ADD_RESOURCE'
 
 export function setResources(resources) {
   return {
@@ -7,11 +9,19 @@ export function setResources(resources) {
     resources,
   }
 }
-export function addGoal(resource) {
+export function addResourcel(resource) {
   return {
     type: ADD_RESOURCE,
     resource,
   }
 }
 
+export function fetchResources(subGoalId) {
+  return (dispatch) => {
+    return getResourcesBySubGoalId(subGoalId).then((data) => {
+      dispatch(setResources(data))
+      return null
+    })
+  }
+}
 export { ADD_RESOURCE, SET_RESOURCES }
