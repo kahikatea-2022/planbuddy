16c84ae872f9c550d1179a761e4cc85fcecf6ec0
Sindre <sinfr91@gmail.com>
2022-05-08 11:23:29 +1200

    add logic for creating new sub goal

--- client/apis/subGoals.js
+++ client/apis/subGoals.js
@@ -5,12 +5,22 @@ const rootUrl = '/api/v1'
 // ?get subgoals by goal id
 export function getSubGoals(goalId) {
   return request
-    .get(rootUrl + `/subgoals/getSubGoals` + goalId)
-    .then((res) => res.body)
+    .get(rootUrl + `/subgoals/getSubGoals/` + goalId)
+    .then((res) => {
+      if (res.body === null) return []
+      return res.body
+    })
 }
 
 export function getSubGoalById(id) {
   return request
     .get(rootUrl + '/subGoals/getSubGoalById/' + id)
     .then((res) => res.body)
+}
+
+export function addSubGoal(goal) {
+  return request
+    .post(rootUrl + '/subGoals/addNewSubgoal')
+    .send(goal)
+    .then((res) => res.body.newSubgoalId[0])
 }
--- server/routes/subGoals.js
+++ server/routes/subGoals.js
@@ -5,10 +5,9 @@ const db = require('../db/sub_goals')
 
 const router = express.Router()
 
-
 // GET /api/v1/subGoals
-router.get('/getSubGoals', (req, res) => {
-  let id = req.body.goalId
+router.get('/getSubGoals:id', (req, res) => {
+  let id = Number(req.params.goalId)
 
   db.getSubGoals(id)
     .then((data) => {
@@ -22,31 +21,35 @@ router.get('/getSubGoals', (req, res) => {
 })
 
 // GET /api/v1/subGoals
-router.get('/getSubGoalById', (req, res) => {
-  let id = req.body.subgoalId
+router.get('/getSubGoalById/:id', (req, res) => {
+  let id = Number(req.params.subgoalId)
 
   db.getSubGoalById(id)
-  .then((data) => {
-    res.json(data)
-    return null
-  })
-  .catch((err) => {
-    console.error(err)
-    res.status(500).json({ message: 'Something went wrong getting the sub goal by subGoal_id' })
-  })
+    .then((data) => {
+      res.json(data)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).json({
+        message: 'Something went wrong getting the sub goal by subGoal_id',
+      })
+    })
 })
 
 router.post('/addNewSubgoal', (req, res) => {
   const newSubgoal = req.body
 
   db.addNewSubGoal(newSubgoal)
-  .then((newSubgoalId) => {
-    res.status(200).json({ newSubgoalId })
-  })
-  .catch((err) => {
-    console.error(err)
-    res.status(500).json({ message: 'Something went wrong creating new subgoal' })
-  })
+    .then((newSubgoalId) => {
+      res.status(200).json({ newSubgoalId })
+    })
+    .catch((err) => {
+      console.error(err)
+      res
+        .status(500)
+        .json({ message: 'Something went wrong creating new subgoal' })
+    })
 })
 
 module.exports = router
