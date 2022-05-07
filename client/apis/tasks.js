32f0195f8174c0e6b98fefe6c27e5108ab0f2484
Sindre <sinfr91@gmail.com>
2022-05-07 15:08:35 +1200

    get task api call

--- client/apis/tasks.js
+++ client/apis/tasks.js
@@ -3,14 +3,8 @@ import request from 'superagent'
 const rootUrl = '/api/v1'
 
 // ?get tasks by subgoal id
-export function getTasks() {
-  return Promise.resolve([
-    { id: 1, name: 'be relational', completed: false, current: true },
-    { id: 2, name: 'be globby', completed: false, current: false },
-    { id: 3, name: 'be rational', completed: true, current: false },
-  ]).then((res) => res)
-
-  // return request.get(rootUrl + '/tasks').then((res) => {
-  //   return res.body.users
-  // })
+export function getTask(id) {
+  return request
+    .get(rootUrl + '/tasks/getTaskById/' + id)
+    .then((res) => res.body)
 }
