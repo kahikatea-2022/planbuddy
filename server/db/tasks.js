157af30742bd0653c7788fd8431f197bc74fb984
thongchai-hon <hon.man@hotmail.com>
2022-05-07 14:52:32 +1200

    add addNewTask db function

--- server/db/tasks.js
+++ server/db/tasks.js
@@ -14,8 +14,21 @@ function getTasksBySubGoalId(subgoal_id, db = connection) {
 function getTaskById(tasks_id, db = connection) {
   return db('tasks').where('task_id', tasks_id).select().first()
 }
+
+function addNewTask(task, db = connection) {
+  const data = {
+    goal_id: task.goalId,
+    subgoal_id: task.subGoalId,
+    task_name: task.taskName,
+    time_spent: task.timeSpent,
+    completed: task.completed,
+    current: task.current,
+  }
+  return db('tasks').insert(data)
+}
 module.exports = {
   getTasks,
   getTasksBySubGoalId,
   getTaskById,
+  addNewTask,
 }
