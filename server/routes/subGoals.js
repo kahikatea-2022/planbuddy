3b0e97e36f7688af4d87cbb78bd77c15ee4f6503
williams-z <williams.z.bis@gmail.com>
2022-05-07 12:02:47 +1200

    added route for add new sub goal

--- server/routes/subGoals.js
+++ server/routes/subGoals.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 //these are the variables to acces the DB functions by table name
-const subGoals = require('../db/sub_goals')
+const db = require('../db/sub_goals')
 
 const router = express.Router()
 
@@ -10,7 +10,7 @@ const router = express.Router()
 router.get('/getSubGoals', (req, res) => {
   let id = req.body.goalId
 
-  subGoals.getSubGoals(id)
+  db.getSubGoals(id)
     .then((data) => {
       res.json(data)
       return null
@@ -25,7 +25,7 @@ router.get('/getSubGoals', (req, res) => {
 router.get('/getSubGoalById', (req, res) => {
   let id = req.body.subgoalId
 
-  subGoals.getSubGoalById(id)
+  db.getSubGoalById(id)
   .then((data) => {
     res.json(data)
     return null
@@ -36,5 +36,17 @@ router.get('/getSubGoalById', (req, res) => {
   })
 })
 
+router.post('/addNewSubgoal', (req, res) => {
+  const newSubgoal = req.body
+
+  db.addNewSubGoal(newSubgoal)
+  .then((newSubgoalId) => {
+    res.status(200).json({ newSubgoalId })
+  })
+  .catch((err) => {
+    console.error(err)
+    res.status(500).json({ message: 'Something went wrong creating new subgoal' })
+  })
+})
 
 module.exports = router
