450b0bccc22c2412e34b61ce77dfce8c347403f6
williams-z <williams.z.bis@gmail.com>
2022-05-07 21:43:56 +1200

    added db function and route for get rflection by task id

--- server/db/reflections.js
+++ server/db/reflections.js
@@ -1,7 +1,23 @@
 //FUNCTIONS RELATED TO REFLECTIONS DB
-
 const connection = require('./connection')
 
-module.exports = {
+
+
+// get reflections by taskId
+function getReflectionsByTaskId(id, db = connection) {
   
+  return db('reflections')
+  .where('task_id', id)
+  .select(
+    'reflection_id as reflectionId',
+    'goal_id as goalId',
+    'task_id as taskId',
+    'reflection'
+  )
+}
+
+
+
+module.exports = {
+  getReflectionsByTaskId,
 }
--- server/routes/reflections.js
+++ server/routes/reflections.js
@@ -5,5 +5,21 @@ const db = require('../db/reflections')
 
 const router = express.Router()
 
+//GET /api/v1/reflections/
+router.get('/getReflectionsByTaskId', (req, res) => {
+  const id = req.body.taskId
+  
+  db.getReflectionsByTaskId(id)
+  .then((data) => {
+    res.json(data)
+    return null
+  })
+  .catch((err) => {
+    console.error(err)
+    res
+      .status(500)
+      .json({ message: 'Something went wrong getting reflection by taskId' })
+  })
+})
 
 module.exports = router
