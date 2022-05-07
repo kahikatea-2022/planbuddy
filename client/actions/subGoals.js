88c2435651ae9d7fb38d1e8b2c938fe307adf379
Sindre <sinfr91@gmail.com>
2022-05-07 14:48:53 +1200

    create fetchsubgoals function

--- client/actions/subGoals.js
+++ client/actions/subGoals.js
@@ -1,3 +1,5 @@
+import { getSubGoals } from '../apis/subGoals'
+
 const SET_SUBGOALS = 'SET_SUBGOALS'
 const ADD_SUBGOAL = 'SET_SUBGOAL'
 
@@ -11,6 +13,14 @@ export function addGoal(subGoal) {
   return {
     type: ADD_SUBGOAL,
     subGoal,
+  }
+}
+
+export function fetchSubGoals(goalId) {
+  return (dispatch) => {
+    return getSubGoals(goalId).then((data) => {
+      dispatch(setSubGoals(data))
+    })
   }
 }
 
