bb4cd5274ef125d8e8705521bd9b5598cbea56a8
thongchai-hon <hon.man@hotmail.com>
2022-05-09 11:53:35 +1200

    add seed data

---
+++ server/db/seeds/quotes.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('table_name').del()
+  await knex('table_name').insert([
+    {
+      quote_id: 1,
+      subject_id: 1,
+      quote: "It's all about the journey, so keep going!",
+    },
+
+    {
+      quote_id: 2,
+      subject_id: 2,
+      quote: 'A goal without a plan is just a wish!',
+    },
+  ])
+}
