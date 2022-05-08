04252609ef318432f93eeefb81448593e4cab0bd
Sindre <sinfr91@gmail.com>
2022-05-09 10:45:17 +1200

    add reflections api

--- client/apis/reflections.js
+++ client/apis/reflections.js
@@ -1,1 +1,16 @@
-//TODO add reflections api calls
+import request from 'superagent'
+
+const rootUrl = '/api/v1'
+
+// ?get resources by subgoal id
+export function getReflectionsByTaskId(id) {
+  return request
+    .get(rootUrl + '/reflections/getReflectionsByTaskId/' + id)
+    .then((res) => res.body)
+}
+export function addNewReflection(reflection) {
+  return request
+    .post(`${rootUrl}/reflections/addNewReflection/`)
+    .send(reflection)
+    .then((res) => res.body)
+}
