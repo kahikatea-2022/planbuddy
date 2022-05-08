f9c3d491bbe4d3bd0ffc507e3dc2be1226f40c6b
thongchai-hon <hon.man@hotmail.com>
2022-05-08 18:25:21 +1200

    edit router function broken

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -5,12 +5,10 @@ const db = require('../db/resources')
 
 const router = express.Router()
 
-
 // GET /api/v1/resources/
 router.get('/getResourcesByGoalId', (req, res) => {
   let id = req.body.goalId
-  db
-    .getResourcesData(id)
+  db.getResourcesData(id)
     .then((data) => {
       res.json(data)
       return null
@@ -27,15 +25,19 @@ router.get('/getResourcesBySubgoalId', (req, res) => {
   const id = req.body.subgoalId
 
   db.getResourcesBySubGoalId(id)
-  .then((data) => {
-    res.json(data)
-    return null
-  })
+    .then((data) => {
+      res.json(data)
+      return null
+    })
 
-  .catch((err) => {
-    console.error(err)
-    res.status(500).json({ message: 'Something went wrong getting resources by subgoalId' })
-  })
+    .catch((err) => {
+      console.error(err)
+      res
+        .status(500)
+        .json({
+          message: 'Something went wrong getting resources by subgoalId',
+        })
+    })
 })
 
 //POST /api/v1/resources
@@ -43,31 +45,51 @@ router.post('/addNewResource', (req, res) => {
   const data = req.body
 
   db.addNewResource(data)
-  .then((newResourceId) => {
-    res.status(200).json({ newResourceId })
-    return null
-  })
-  .catch((err) => {
-    console.error(err)
-    res.status(500).json({ message: 'Something went wrong getting resources by subgoalId' })
-  })
+    .then((newResourceId) => {
+      res.status(200).json({ newResourceId })
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res
+        .status(500)
+        .json({
+          message: 'Something went wrong getting resources by subgoalId',
+        })
+    })
 })
 
 //PATCH /api/v1/resources
-router.patch('/editResource')
+router.patch('/editResource', (req, res) => {
+  console.log('reouter hit')
+  const data = req.body
+  db.editResource(data)
+    .then(() => {
+      res.sendStatus(200)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res
+        .status(500)
+        .json({ message: 'Something went wrong editing the resource' })
+    })
+})
 
 //DELETE /api/v1/resources
 router.delete('/deleteResourceById', (req, res) => {
   const resourceId = req.body.resourceId
 
   db.deleteResourceById(resourceId)
-  .then(() => {
-    res.status(200).json({ message: 'resource deleted successfully' })
-  })
-  .catch((err) => {
-    console.error(err)
-    res.status(500).json({ message: 'there was an error deleting the resource' })
-  })
+    .then(() => {
+      res.status(200).json({ message: 'resource deleted successfully' })
+    })
+    .catch((err) => {
+      console.error(err)
+      res
+        .status(500)
+        .json({ message: 'there was an error deleting the resource' })
+    })
 })
 
 module.exports = router
