fd33e6ef561c144afa1db92516e91f7a430d6236
Sindre <sinfr91@gmail.com>
2022-05-09 15:19:01 +1200

    add controlled input

--- client/components/Reflections.jsx
+++ client/components/Reflections.jsx
@@ -1,24 +1,64 @@
 import { format } from 'prettier'
 import React, { useEffect, useState } from 'react'
+import { useDispatch , useSelector } from 'react-redux'
 // import { useSelector, useDispatch } from 'react-redux'
 // import { addGoal, ADD_GOAL, fetchGoals } from '../actions/goals'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { fetchSubGoal } from '../actions/subGoals'
+import { fetchTask } from '../actions/tasks'
+import { addNewReflection } from '../apis/reflections'
 // import { addResource } from '../actions/resources'
 // import { addTask, setTasks } from '../actions/tasks'
 
 function Reflections() {
+const navigate = useNavigate()
+const dispatch = useDispatch()
+const task = useSelector(state=>state.task)
+const subgoal = useSelector(state=>state.subgoal)
+const {taskId} = useParams()
+
+const [reflection, setReflection] = useState('')
+
+useEffect(()=>{
+  dispatch(fetchTask(Number(taskId)))
+}, [])
+
+useEffect(()=>{
+  dispatch(fetchSubGoal(task.subgoalId))
+}, [task])
+
+
+function inputHandler(e){
+  setReflection(e.target.value)
+  console.log(reflection)
+}
+
+function logoutAndComplete(e){
+  e.preventDefault()
+  const newReflection = {
+    subgoalId: subgoal.subgoalId,
+    taskId: task.taskId,
+    reflection: reflection
+  }
+  addNewReflection()
+}
+
+function toGoalsAndComplete(e){
+  e.preventDefault()
+  
+}
   return (
     <>
       <h1>Reflections</h1>
       <div className="subGoalCreator">
         {/* <a href='' > */}
         <img src="/images/Pencil.png"></img>
-        <p>*Learn C Major Scale*</p>
+        <p>{task.taskName}</p>
         {/* </a> */}
       </div>
-      <textarea rows="5" cols="50"></textarea>
-      <button>Complete Reflection and Log out</button>
-      <button>Complete Reflection and return to learning plan</button>
+      <textarea onChange={inputHandler} value={reflection} rows="5" cols="50"></textarea>
+      <button onClick={logoutAndComplete}>Complete Reflection and Log out</button>
+      <button onClick={toGoalsAndComplete}>Complete Reflection and return to learning plan</button>
     </>
   )
 }
