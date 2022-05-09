6d9b0c2bec69c09773e681e63392548278929a24
Sindre <sinfr91@gmail.com>
2022-05-10 00:00:47 +1200

    fix conflict

--- client/components/ResourcesListItem.jsx
+++ client/components/ResourcesListItem.jsx
@@ -2,6 +2,6 @@ import React, { useState } from 'react'
 
 export function ResourcesListItem({resource}) {
   return (
-    <li><a target="_blank" href={resource.url} rel="noreferrer">{resource.resourceName}</a></li>
+    <li><a className="list" target="_blank" href={resource.url} rel="noreferrer">{resource.resourceName}</a></li>
   )
 }
