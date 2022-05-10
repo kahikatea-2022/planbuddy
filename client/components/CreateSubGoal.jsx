d875b97bdebeca5249e3d0b0e5571cd0fdbb8f71
Sindre <sinfr91@gmail.com>
2022-05-10 16:47:04 +1200

    conditional complete render

--- client/components/CreateSubGoal.jsx
+++ client/components/CreateSubGoal.jsx
@@ -35,6 +35,7 @@ function CreateSubGoal({first, schugl}) {
   // console.log(resources)
   const [checkboxState, setCheckboxState] = useState(false)
   const [checked, setChecked] = useState(false)
+  const [complete, setComplete] = useState(false)
   const [inputStateResources, setInputStateResources] = useState({
     resourceName: '',
     url: '',
@@ -54,6 +55,9 @@ function CreateSubGoal({first, schugl}) {
     dispatch(fetchResources(Number(subgoalId)))
     dispatch(fetchTasks(Number(subgoalId)))
   },[])
+  useEffect(()=>{
+    checkCompletion(tasks)
+  },[tasks])
   // Validate ownership, needs slight rework to accout for inital empty data
   // useEffect(()=>{
   //   getGoalsByUserId(user.id).then(res=>{
@@ -133,6 +137,13 @@ function CreateSubGoal({first, schugl}) {
     }
   }
 
+  function checkCompletion(tasks){
+    const notComplete = tasks.find(el=>el.completed == 0)
+    if(notComplete) return setComplete(false)
+    return setComplete(true)
+
+  }
+
   //input have a default value of subgoal name,
   //when you press enter, the name of the subgoal needs to be updated in the database via patch route
   //updateSubgoalById
@@ -178,30 +189,11 @@ function CreateSubGoal({first, schugl}) {
         </form>
 
         <button onClick={submitHandlerResources}>Add Resource</button>
-        {/* list to render the resources*/}
-        {/* <ul>
-        <li>{resources.resource_name}</li>
-      </ul> */}
       <form>
         {/* this needs to change based on whether subgoal has been created */}
         <h2>{first?"Great work, now add your first tasks":"Tasks:"}</h2>
         <ul>
-          {<TaskList tasks={tasks}/>}
-          {/* {tasks.map((task) => {
-            return (
-              <li key={task.taskName + task.taskId}>
-                <input
-                  onChange={() => checkboxHandler(task)}
-                  type={'checkbox'}
-                  defaultChecked={task.completed}
-                  checked={checked}
-                />
-                <span onClick={() => goToTaskHandler(task.taskId)}>
-                  {task.taskName}
-                </span>
-              </li>
-            )
-          })} */}
+          {<TaskList tasks={tasks} check={checkCompletion}/>}
         </ul>
         <input
           className="textbox-input"
@@ -213,7 +205,7 @@ function CreateSubGoal({first, schugl}) {
         ></input>
         <button onClick={submitHandlerTasks}>Add New Task</button>
       </form>
-      <button onClick={completeHandler}>Complete Subgoal</button>
+      {complete && !first && <button onClick={completeHandler}>Complete Subgoal</button>}
 
       <PlanBuddy />
     </div>
@@ -221,23 +213,3 @@ function CreateSubGoal({first, schugl}) {
   )
 }
 export default CreateSubGoal
-
-{
-  /*
- subgoal_id: 1,
-      goal_id: 1,
-      subgoal_name: 'learn C major scale',
-      reward_id: 1,
-      completed: false,
-      current: true
-
-{
-      resource_id: 1,
-      goal_id: 1, 
-      subgoal_id: 1,
-      resource_name: 'youtube',
-      url: 'www.youtube.com'
-    }, 
-
-*/
-}
--- client/components/TaskList.jsx
+++ client/components/TaskList.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {TaskListItem} from './TaskListItem'
 
-export function TaskList({tasks}){
+export function TaskList({tasks, check}){
   return (
     <>
-    {tasks && tasks.map(task=><TaskListItem key={task.taskId} task={task}/>)}
+    {tasks && tasks.map(task=><TaskListItem key={task.taskId} task={task} tasks={tasks} check={check}/>)}
     </>
+    
   )
 }
