e90b07bf3b4ab3d0193679b78e84043dbf99120e
Sindre <sinfr91@gmail.com>
2022-05-26 14:26:52 +1200

    make fix user Id

--- client/components/VeteranView.jsx
+++ client/components/VeteranView.jsx
@@ -13,11 +13,11 @@ function VeteranView() {
   const task = useSelector((state) => state.task)
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  dispatch(updateUser(addUserId(user)))
 
   //not returning the current task
   useEffect(() => {
     dispatch(fetchTask(Number(user.currentTask)))
-    dispatch(updateUser(addUserId(user)))
   }, [user])
 
   //site doesnt know what user.currentTask is
